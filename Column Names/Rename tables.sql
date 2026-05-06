9581e46478b1965453fbd7502fef105f04387874
mactrias <73613066+mactrias@users.noreply.github.com>
2026-05-06 16:31:29 -0300

    2 ej

---
+++ Column Names/Rename tables.sql
@@ -0,0 +1,3 @@
+--- Ejercicio 2, rename columns from a table
+
+SELECT emp_no AS "Employee #", birth_date AS "Birthday", first_name AS "FisrstName" FROM public.employees 
---
+++ Query/query.sql
@@ -0,0 +1,8 @@
+-- Ejercicio 1: SELECT básicos
+-- Objetivo: consultar tablas principales
+
+
+SELECT * FROM public.employees ; 
+SELECT * FROM public.departments;
+SELECT * FROM public.salaries;
+SELECT * FROM public.titles WHERE emp_no = 10006;
