c831ed4a6dc44a7e681d053645d72591673a0083
mactrias <73613066+mactrias@users.noreply.github.com>
2026-05-12 19:05:54 -0300

    aggregate

---
+++ Aggregate Func/Aggregate functions.sql
@@ -0,0 +1,11 @@
+--- aggregate functions
+
+
+
+SELECT count(emp_no) FROM employees;
+SELECT min(emp_no) FROM employees;
+SELECT max(emp_no) FROM employees;
+
+SELECT max(salary) FROM salaries;
+
+SELECT sum(salary) FROM salaries;
