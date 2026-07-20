bd83f0886e5f3f9b5955d6fbb0de33f76030655b
mactrias <73613066+mactrias@users.noreply.github.com>
2026-07-19 22:27:26 -0300

    wf

--- windiwfunct/wf.sql
+++ windiwfunct/wf.sql
@@ -17,3 +17,66 @@ JOIN departments d
 WHERE s.to_date = '9999-01-01'
   AND de.to_date = '9999-01-01'
 ORDER BY s.emp_no;
+
+
+/*
+*  Show the population per continent
+*  Database: World
+*  Table: Country
+*/
+
+SELECT DISTINCT
+    continent,
+    SUM(population) OVER (
+        PARTITION BY continent
+    ) AS continent_population
+FROM country;
+
+
+
+/*
+*  To the previous query add on the ability to calculate the percentage of the world population
+*  What that means is that you will divide the population of that continent by the total population and multiply by 100 to get a percentage.
+*  Make sure you convert the population numbers to float using `population::float` otherwise you may see zero pop up
+*  Try to use CONCAT AND ROUND to make the data look pretty
+*
+*  Database: World
+*  Table: Country
+*/
+
+
+SELECT DISTINCT
+    continent,
+    SUM(population) OVER (
+        PARTITION BY continent
+    ) AS continent_population,
+
+    CONCAT(
+        ROUND(
+            (
+                SUM(population::FLOAT) OVER (
+                    PARTITION BY continent
+                )
+                /
+                SUM(population::FLOAT) OVER ()
+            ) * 100
+        ),
+        '%'
+    ) AS continent_percentage
+
+FROM country;
+
+
+
+/*
+*  Count the number of towns per region
+*
+*  Database: France
+*  Table: Regions (Join + Window function)
+*/
+
+SELECT DISTINCT "r"."name", count( t.id ) OVER(PARTITION BY r.code) AS "town per region"
+FROM regions AS r
+JOIN departments AS d ON r.code = d.region
+JOIN towns AS t ON d.code = t.department
+ORDER BY "town per region"
